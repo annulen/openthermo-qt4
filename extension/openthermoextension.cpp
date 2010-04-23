6bf4e1647cbdd49044128882f8ce6b0c95180eb2
annulen <annulen@yandex.ru>
2010-04-23 19:16:38 +0400

    Stub

---
+++ extension/openthermoextension.cpp
@@ -0,0 +1,81 @@
+
+#include "openthermoextension.h"
+
+#include <QtGui/QAction>
+
+#include <avogadro/glwidget.h>
+#include <avogadro/molecule.h>
+
+namespace Avogadro
+{
+  OpenThermoExtension::OpenThermoExtension(QObject *parent) : Extension(parent)
+                                                              m_molecule(0),
+                                                              m_dialog(0),
+                                                              m_widget(0)
+  {
+    QAction *action = new QAction( this );
+    action->setText( tr("OpenThermo" ));
+    m_actions.append( action );
+  }
+  
+  OpenThermoExtension::~OpenThermoExtension()
+  {}
+  
+  QList<QAction *> OpenThermoExtension::actions() const
+  {
+    return m_actions;
+  }
+  
+  QString OpenThermoExtension::menuPath(QAction *action) const
+  {
+    Q_UNUSED(action)
+    return tr("&Build");
+  }
+  
+  void OpenThermoExtension::setMolecule(Molecule *molecule)
+  {
+    if (m_molecule)
+      disconnect( m_molecule, 0, 0, 0 );
+
+    m_molecule = molecule;
+
+    if (m_dialog) {
+      m_dialog->setMolecule(molecule);
+    }
+  } 
+  
+  QUndoCommand* OpenThermoExtension::performAction( QAction *action, GLWidget *widget )
+  {
+    Q_UNUSED(action)
+    QUndoCommand *undo = 0;
+    
+    if (!m_molecule)
+      return 0;
+
+    // Disconnect in case we're attached to a new widget
+    if (m_widget) {
+      disconnect( m_molecule, 0, 0, 0 );
+      if (m_dialog) {
+        m_dialog->setMolecule(m_molecule);
+      }
+    }
+
+    if (widget)
+      m_widget = widget;    
+
+    if (!m_dialog) {
+      m_dialog = new QDialog(m_widget);
+      QVBoxLayout *layout = new QVBoxLayout(dialog);
+      dialog->setLayout(layout);
+      if (!m_otWidget)
+        m_otWidget = new OpenThermoWidget(m_dialog);
+      layout->addWidget(m_otWidget);
+      dialog->setWindowTitle("OpenThermo");      
+    }
+    m_dialog->show();
+    return undo;
+  }
+
+} // end namespace Avogadro
+
+Q_EXPORT_PLUGIN2(openthermoextension, Avogadro::OpenThermoExtensionFactory)
---
+++ extension/openthermoextension.hpp
@@ -0,0 +1,49 @@
+#ifndef OPENTHERMOEXTENSION_H
+#define OPENTHERMOEXTENSION_H
+
+#include <AvogadroWidget/extension.h>
+#include "../common/openthermowidget.hpp"
+
+namespace Avogadro {
+
+  class OpenThermoExtension : public Extension
+  {
+    Q_OBJECT
+      AVOGADRO_EXTENSION("OpenThermo", tr("OpenThermo"),
+                         tr("OpenThermo integration"))
+
+    public:
+      //! Constructor
+      OpenThermoExtension(QObject *parent=0);
+      //! Deconstructor
+      virtual ~OpenThermoExtension();
+
+      virtual QList<QAction *> actions() const;
+      virtual QString menuPath(QAction *action) const;
+
+      virtual QDockWidget * dockWidget();
+      virtual QUndoCommand* performAction(QAction *action, GLWidget *widget);
+
+      virtual void setMolecule(Molecule *molecule);
+
+    private:
+      QList<QAction *> m_actions;
+      Molecule *m_molecule;
+	  QDialog *m_dialog;
+	  OpenThermoWidget *m_otWidget;
+	  GLWidget *m_glWidget;
+
+    private Q_SLOTS:
+
+  };
+
+  class OpenThermoExtensionFactory : public QObject, public PluginFactory
+  {
+    Q_OBJECT
+    Q_INTERFACES(Avogadro::PluginFactory)
+    AVOGADRO_EXTENSION_FACTORY(OpenThermoExtension)
+  };
+
+} // end namespace Avogadro
+
+#endif
